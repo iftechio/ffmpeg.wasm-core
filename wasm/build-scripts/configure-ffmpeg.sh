1e64b9387b5852f058f3eb250963bd86edd8140d
wong2 <wonderfuly@gmail.com>
2021-06-03 08:53:50 +0000

    Add aac demuxer

--- wasm/build-scripts/configure-ffmpeg.sh
+++ wasm/build-scripts/configure-ffmpeg.sh
@@ -16,7 +16,7 @@ FLAGS=(
   --enable-decoder=pcm_f32le,pcm_f32be,pcm_f64le,pcm_f64be,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,pcm_s24be,pcm_s24le,pcm_s24le_planar,pcm_s32be,pcm_s32le,pcm_s32le_planar,pcm_s8,pcm_s8_planar,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8
   --enable-encoder=pcm_f32le,pcm_f32be,pcm_f64le,pcm_f64be,pcm_s16be,pcm_s16be_planar,pcm_s16le,pcm_s16le_planar,pcm_s24be,pcm_s24le,pcm_s24le_planar,pcm_s32be,pcm_s32le,pcm_s32le_planar,pcm_s8,pcm_s8_planar,pcm_u16be,pcm_u16le,pcm_u24be,pcm_u24le,pcm_u32be,pcm_u32le,pcm_u8
   --enable-muxer=fifo,flv,mp3,mp4,mov,m4v,matroska,ogg,oga,opus,wav,ipod
-  --enable-demuxer=fifo,flv,mp3,mp4,mov,m4v,matroska,ogg,oga,opus,wav,ipod
+  --enable-demuxer=fifo,flv,mp3,mp4,mov,m4v,matroska,ogg,oga,opus,wav,ipod,aac
   --enable-filter=acompressor,acopy,anull,format,aformat,null,transpose,trim,acue,acrossfade,acrossover,adelay,adenorm,aemphasis,afade,afreqshift,aloop,amerge,amix,aresample,asplit,atrim
   --enable-filter=channelmap,channelsplit,chorus,crossfeed,dynaudnorm,equalizer,silencedetect,silenceremove,speechnorm,volume
   --enable-gpl            # required by x264
